7b515571d1934600ec1a469f49f4c5e98bdf02ce
syed_umair98 <syed.umair.anwar@antronsys.com>
2020-12-14 00:41:06 +0530

    Added:sorting is done and get second smallest and largest number

--- ten_random_no.js
+++ ten_random_no.js
@@ -4,26 +4,10 @@ for (let i = 0; i < 10; i++) {
     array.push(ran)
 
 }
-let large = 1
-let second_large = 0
-let small = 999
-let second_small = 998
-for (let i = 0; i < array.length; i++) {
-    if (array[i] > large) {
-        second_large = large
-        large = array[i]
-    }
-    else if (array[i] > second_large && array[i] != large) {
-        second_large = array[i]
-    }
-    else if (array[i] < small) {
-       second_small = small
-        small = array[i]
-    }
-    else if (array[i] < second_small && array[i] != small) {
-        second_small = array[i]
-    }
-}
 console.log(array)
-console.log("Second large is ", second_large)
+array=array.sort()
+console.log(array)
+let second_small=array[1]
+let second_large=array[array.length-2]
 console.log("Second small is ",second_small)
+console.log("Second large is ", second_large)
